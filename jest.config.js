4b9dd7f39a76157073db386941a8f93b93a24be2
Lila Papazian <lilapapazian@gmail.com>
2025-06-29 23:18:03 -0300

    add jest configurations

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
